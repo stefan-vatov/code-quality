c44bd3b4b61fe60e3ab326bfa00358b0e9b30e49
Stefan Vatov <stefan@thethracian.net>
2026-05-25 03:16:36 +0100

    Convert acronym set composition to Effect

--- ts/src/rules/acronyms.ts
+++ ts/src/rules/acronyms.ts
@@ -4,16 +4,14 @@
 /*  Common source code abbreviations. Stored lowercase for case-insensitive   */
 /*                                Set lookup.                                 */
 /* -------------------------------------------------------------------------- */
+import { Array } from 'effect';
 import acronyms1 from './acronyms-1';
 import acronyms2 from './acronyms-2';
 import acronyms3 from './acronyms-3';
 import acronyms4 from './acronyms-4';
 
-const acronyms: ReadonlySet<string> = new Set([
-  ...acronyms1,
-  ...acronyms2,
-  ...acronyms3,
-  ...acronyms4,
-]);
+const acronymGroups = [acronyms1, acronyms2, acronyms3, acronyms4] as const;
+
+const acronyms: ReadonlySet<string> = new Set(Array.flatten(acronymGroups));
 
 export default acronyms;
